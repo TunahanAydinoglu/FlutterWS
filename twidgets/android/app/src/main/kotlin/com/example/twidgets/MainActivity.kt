5344b1f0b22d1166ebd8d3f8b85fa7208f83f369
Tunahan Aydinoglu <tunahanaydinoglu@outlook.com>
2020-09-08 15:24:22 +0300

    Flexible

---
+++ twidgets/android/app/src/main/kotlin/com/example/twidgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.twidgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ twidgets/lib/twidgets/tflexible.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TFlexible extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Flexible Deneme")),
+      ),
+      body: Container(
+        color: Colors.grey,
+        child: Row(
+          children: [
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 5,
+              child: RaisedButton(
+                child: Text("New Button"),
+                onPressed: () {},
+              ),
+            ),
+            Flexible(fit: FlexFit.tight, flex: 1, child: SizedBox()),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 10,
+              child: RaisedButton(
+                color: Colors.blue,
+                child: Text("New Button"),
+                onPressed: () {},
+              ),
+            ),
+            Flexible(fit: FlexFit.tight, flex: 1, child: SizedBox()),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 5,
+              child: RaisedButton(
+                child: Text("New Button"),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
